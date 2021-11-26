50d11a70f7b19f75314f4251fc53d5b1b02f2cf5
ampirzadeh <am.pirzadeh@gmail.com>
2021-11-26 19:04:37 +0000

    Deploying to gh-pages from  @ faa0adea95786ed015d762342b913c737fd54675 🚀

--- _nuxt/static/1637136090/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fzahra\u002F_nuxt\u002Fstatic\u002F1637136090",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fzahra\u002F",assetsPath:"\u002Fzahra\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e88c5dd1"}}}}(null));
